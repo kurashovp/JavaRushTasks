4ced3a567153ef4ad98edc765faa336fb96c2413
Pavel Kurashov <kurashovp@gmail.com>
2021-01-06 22:21:32 +0700

    task 2017 refactor to visualize Directions enum

--- 2.JavaCore/src/com/javarush/task/task20/task2027/Direction.java
+++ 2.JavaCore/src/com/javarush/task/task20/task2027/Direction.java
@@ -1,14 +1,12 @@
 package com.javarush.task.task20.task2027;
 
 public enum Direction {
-    EE(0, 1),
-    WW(0, -1),
-    SS(1, 0),
-    NN(-1, 0),
-    EN(-1, 1),
-    ES(1, 1),
-    WS(1, -1),
-    WN(-1, -1);
+
+    WN(-1, -1), NN(-1, 0), EN(-1, 1),
+    /* ↖ ↑ ↗ */
+    WW(0, -1), /* ←   → */  EE(0, 1),
+    /* ↙ ↓ ↘ */
+    WS(1, -1), SS(1, 0), ES(1, 1);
 
     private final int i;
     private final int j;
